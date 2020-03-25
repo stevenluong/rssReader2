ecd04f951411cc08d3b613b6a07dc61393a375d0
Steven Luong <sluong@dalet.com>
2020-03-25 11:51:58 +1100

    Not found

--- reactjs/src/AppWithRouterAccess.js
+++ reactjs/src/AppWithRouterAccess.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Route, useHistory } from 'react-router-dom';
+import { Route, useHistory, Switch } from 'react-router-dom';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import SignInSide from './SignInSide';
 import SignUp from './SignUp';
 import Main from './Main';
 import Profile from './Profile';
 import config from './app.config';
+import Notfound from './NotFound'
 
 const AppWithRouterAccess = () => {
   const history = useHistory();
@@ -19,11 +20,14 @@ const AppWithRouterAccess = () => {
               redirectUri={config.redirectUri}
               onAuthRequired={onAuthRequired}
               pkce={true} >
-      <SecureRoute path='/' exact={true} render={() => <Main url="dashboard" />} />
-      <Route path='/login' render={() => <SignInSide baseUrl={config.url} />} />
-      <Route path='/signup' render={() => <SignUp baseUrl={config.url} />} />
-      <Route path='/implicit/callback' component={LoginCallback} />
-      <SecureRoute path='/profile' render={() => <Main url="profile" />} />
+      <Switch>
+        <SecureRoute path='/' exact={true} render={() => <Main url="dashboard" />} />
+        <Route path='/login' render={() => <SignInSide baseUrl={config.url} />} />
+        <Route path='/signup' render={() => <SignUp baseUrl={config.url} />} />
+        <Route path='/implicit/callback' component={LoginCallback} />
+        <SecureRoute path='/profile' render={() => <Main url="profile" />} />
+        <Route component={Notfound} />
+      </Switch>
     </Security>
   );
 };
